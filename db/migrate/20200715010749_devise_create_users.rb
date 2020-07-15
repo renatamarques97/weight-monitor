1f507d29fe1b045ad2f586110166cbc133d6b17a
Renata Marques <renata.nrmarques@gmail.com>
2020-07-15 19:23:14 -0300

    users modified

--- db/migrate/20200715010749_devise_create_users.rb
+++ db/migrate/20200715010749_devise_create_users.rb
@@ -4,7 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "", unique: true
+      t.string :name,               null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
