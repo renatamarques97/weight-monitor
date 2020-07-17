46bdaf022b32a4cf1b9c12c11d076ab0b04b411b
Renata Marques <renata.nrmarques@gmail.com>
2020-07-16 22:34:40 -0300

    fixing spec

--- spec/factories/meals.rb
+++ spec/factories/meals.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :meal do
     schedule { "2020-07-15 20:02:58" }
-    description { FFaker::Lorem.paragraphs }
+    description { "Description" }
     meal_type { rand(0..3) }
     association :diet
   end
