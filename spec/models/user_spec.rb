674269613c5b49778ae436d9fbf43839c7e9971d
Renata Marques <renata.nrmarques@gmail.com>
2020-07-18 13:12:17 -0300

    add weight association on user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "relations" do
     it { is_expected.to have_many(:diets).dependent(:destroy) }
+    it { is_expected.to have_many(:weights).dependent(:destroy) }
   end
 
   describe "validations" do
