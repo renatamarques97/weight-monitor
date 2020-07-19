9cddbfc905a7e84d480b5a683c215fd8a13e830e
Renata Marques <renata.nrmarques@gmail.com>
2020-07-19 16:33:15 -0300

    imc and weights for the chart

---
+++ app/presenters/imc_presenter.rb
@@ -0,0 +1,20 @@
+class ImcPresenter
+  attr_reader :user
+
+  def initialize(user)
+    @user = user
+  end
+
+  def call
+    return 0 if weight.nil? || height.nil?
+    (weight / (height)**2).round(2)
+  end
+
+  def weight
+    user.weights.last.try(:kg)
+  end
+
+  def height
+    user.diets.last.try(:height)
+  end
+end
