c6cd787f78ec57f4e0dcb3c4e39cf83fdf062f1b
Renata Marques <renata.nrmarques@gmail.com>
2020-07-19 19:53:22 -0300

    name permitted in devise

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_214445) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "name", default: "", null: false
+    t.string "name", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
