1414a03a35cd0def7fa9db104d76c9ccc300f0ac
Renata Marques <renata.nrmarques@gmail.com>
2024-02-05 22:25:34 -0300

    update weight type from integer to float

--- db/schema.rb
+++ db/schema.rb
@@ -2,28 +2,27 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_214445) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_02_06_011711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "diets", force: :cascade do |t|
     t.date "start_date", default: "2020-07-19", null: false
     t.date "end_date", default: "2020-07-19", null: false
-    t.integer "initial_weight", null: false
-    t.integer "target_weight", null: false
+    t.float "initial_weight", null: false
+    t.float "target_weight", null: false
     t.float "height"
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_diets_on_user_id"
   end
 
@@ -32,8 +31,8 @@ ActiveRecord::Schema.define(version: 2020_07_17_214445) do
     t.text "description", null: false
     t.integer "meal_type", null: false
     t.bigint "diet_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["diet_id"], name: "index_meals_on_diet_id"
   end
 
@@ -44,8 +43,8 @@ ActiveRecord::Schema.define(version: 2020_07_17_214445) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -54,8 +53,8 @@ ActiveRecord::Schema.define(version: 2020_07_17_214445) do
     t.float "kg", null: false
     t.date "weight_date", default: "2020-07-19", null: false
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_weights_on_user_id"
   end
 
