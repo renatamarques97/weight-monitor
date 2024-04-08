bc5131ede11fc74bdf27875cd205dd566b23a638
Renata Marques <renata.nrmarques@gmail.com>
2024-04-08 08:20:32 -0300

    add open ai integration with hotwire

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/chat_controller.js
+//= link controllers/index.js
