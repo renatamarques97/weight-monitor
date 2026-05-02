8046b6bbf08cd630b3bc3ff5b81f77454d8ef90b
Renata <renata@Renatas-MacBook-Air.local>
2026-05-02 15:58:17 -0300

    improve sass in order to run the specs

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module WeightMonitor
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths << Rails.root.join("app/services")
+    config.eager_load_paths << Rails.root.join("app/services")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
