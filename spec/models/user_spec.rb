24467f92169b58e571a9aa5349533eb8f71eab23
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2026-05-07 16:06:48 -0300

    test: update existing specs for workout refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe "relations" do
     it { is_expected.to have_many(:diets).dependent(:destroy) }
     it { is_expected.to have_many(:weights).dependent(:destroy) }
+    it { is_expected.to have_many(:workouts).dependent(:destroy) }
   end
 
   describe "validations" do
@@ -11,5 +12,25 @@ RSpec.describe User, type: :model do
 
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  end
+
+  describe "workout helpers" do
+    let(:user) { create(:user) }
+
+    it "defines singular and plural methods for each workout type" do
+      WorkoutType::MAPPING.keys.each do |workout_type|
+        expect(user).to respond_to(workout_type)
+        expect(user).to respond_to(workout_type.to_s.pluralize)
+      end
+    end
+
+    it "returns workouts filtered by type" do
+      create(:running, user: user)
+      create(:running, user: user)
+      create(:cycling, user: user)
+
+      expect(user.runnings.count).to eq(2)
+      expect(user.cyclings.count).to eq(1)
+    end
   end
 end
