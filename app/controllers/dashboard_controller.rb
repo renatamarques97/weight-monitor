36084995abacc16084cc92736fe79f3d09b29016
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2026-05-07 16:06:16 -0300

    feat: update dashboard with workout charts and period filter

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,10 +4,12 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @period_in_days = params[:period_in_days]&.to_i || 30
+    @active_tab = params[:active_tab] || 'weight'
     @goal = ::GoalPresenter.new(current_user).achieved?
     @diets = ::Diet.authorized_user(current_user)
     @imc = ::ImcPresenter.new(current_user).call
-    @weights = ::WeightQuery.weights(current_user)
-    @runnings = ::RunningQuery.runnings(current_user)
+    @weights = ::WeightQuery.weights(current_user, @period_in_days)
+    @workout_charts = ::WorkoutQuery.chart_data(current_user, @period_in_days)
   end
 end
