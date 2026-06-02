85e322f41777dcb4069a2ac5da5122f049cba566
Renata Marques <renata.nrmarques@gmail.com>
2026-06-01 22:14:07 -0300

    add fat secret route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-
 Rails.application.routes.draw do
   resources :weights, only: [:index, :new, :create]
   resources :meals
   root to: 'dashboard#index'
   resources :workouts
   resources :diets
+  resources :fatsecret_foods, only: [:index, :show]
   devise_for :users
   resources :chats, only: [:index]
   resource :chat_responses, only: [:show]
