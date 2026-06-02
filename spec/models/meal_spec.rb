416be8e251513aef8e9b1a43df10c1fc5475fb80
Renata Marques <renata.nrmarques@gmail.com>
2026-06-02 20:03:33 -0300

    add relation to exitent specs

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Meal, type: :model do
   describe "relations" do
-    it { is_expected.to belong_to(:diet) }
+    it { is_expected.to belong_to(:diet).optional }
+    it { is_expected.to belong_to(:meal_diary).optional }
+    it { is_expected.to have_many(:meal_foods).dependent(:destroy) }
   end
 
   describe "validations" do
@@ -10,5 +12,23 @@ RSpec.describe Meal, type: :model do
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_presence_of(:meal_type) }
     it { is_expected.to enumerize(:meal_type) }
+
+    it 'is invalid without diet and meal_diary' do
+      meal = build(:meal, diet: nil, meal_diary: nil)
+
+      expect(meal).not_to be_valid
+      expect(meal.errors[:base]).to include('Meal must belong to a diet or meal diary')
+    end
+
+    it 'is valid with meal_diary when diet is nil' do
+      meal_diary = create(:meal_diary)
+      meal = build(:meal, diet: nil, meal_diary: meal_diary)
+
+      expect(meal).to be_valid
+    end
+  end
+
+  describe 'nested attributes' do
+    it { is_expected.to accept_nested_attributes_for(:meal_foods) }
   end
 end
