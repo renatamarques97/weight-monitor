7d43a179b5d638b288f97279735ff0373f8c236e
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2026-06-04 16:51:46 -0300

    fix params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :height)}
 
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :height)}
+  end
 
   private
 
